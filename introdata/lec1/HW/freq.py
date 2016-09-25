d4a70d565029729c98e23bd96db5c76cc8e4f23c
Dima Sheviakov <shedi26@mail.ru>
2016-09-25 13:50:35 +0300

    introdata hw 1 done

--- introdata/lec1/HW/freq.py
+++ introdata/lec1/HW/freq.py
@@ -1,4 +1,10 @@
 #coding=UTF-8
+
+import sys
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+
 count = {
     u'у':0, 
     u'е':0, 
@@ -18,12 +24,26 @@ def print_freq(d, t):
     for k in d.keys():
     	freq = d[k]/1.0/t
         print(k.encode('utf8')+': '+str(freq))
+
+try:
+    filename = sys.argv[1]
+except:
+    filename = 'input.txt'
     
-with open('input.txt', 'r') as f:
-    for line in f:
-        for ch in line.decode('utf8'):
-            if ch.isalnum() : total += 1
-            if ch in count.keys(): count[ch] += 1
+try:
+    with open(filename, 'r') as f:
+        for line in f:
+            for ch in line.decode('utf8').lower():
+                if ch.isalnum() : total += 1
+                if ch in count.keys(): count[ch] += 1
+except:
+    print('Error of reading '+filename)
+    exit()
 
 print('Частотность русских гласных в тексте:')
 print_freq(count, total)
+
+matplotlib.rc('font', family= 'Arial, Liberation Sans')
+plt.bar(np.arange(len(count)), count.values())
+plt.xticks(np.arange(len(count)), count.keys())
+plt.show()
